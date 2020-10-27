321154f5f57debd6e909d6bde9ca769dba95b1c1
16010948 <bjyoung0@gmail.com>
2020-10-27 20:19:48 +0900

    [feat] sequelize server와 연결 - sequelize server와 연결

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,5 +1,9 @@
+import Comment, { associate as associateComment } from './comment';
+
 export * from './sequelize';
 
-const db = {};
+const db = {Comment};
 
 export type dbType = typeof db;
+
+associateComment(db);
