1a44e5c6322071dd1338775da1c8fc88e1ed8e84
Baek Jiyoung <bjyoung0@gmail.com>
2020-10-29 15:17:53 +0900

    [feat] router 등록 - router index에 lables router 등록

--- server/src/api/routes/index.ts
+++ server/src/api/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import issueRouter from './issue';
+import labelsRouter from './labels';
 
 const router = Router();
 
 router.use('/issue', issueRouter);
+router.use('/labels', labelsRouter);
 
 export default router;
