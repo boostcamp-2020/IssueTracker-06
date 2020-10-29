d757a00c2cda52e80416e66c99482eff4381b5e7
Eunbin <qpmz1212@naver.com>
2020-10-29 15:18:13 +0900

    [feat] comment router 등록 - comment 관련 API routing 해주는 comment router 등록

--- server/src/api/routes/index.ts
+++ server/src/api/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import issueRouter from './issue';
 import labelRouter from './label';
+import commentRouter from './comment';
 
 const router = Router();
 
 router.use('/issue', issueRouter);
 
 router.use('/label', labelRouter);
+
+router.use('/comment', commentRouter);
 
 export default router;
