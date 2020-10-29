e504ed6a92a6381924a4145ea18b9822a2e0768f
Baek Jiyoung <bjyoung0@gmail.com>
2020-10-29 17:05:13 +0900

    [feat] milestone 상태 변경 controller 작성 - PUT milestone status API controller 작성 - milestone controller index에 update milestone status controller 등록
    #25

--- server/src/api/routes/milestone/controller/index.ts
+++ server/src/api/routes/milestone/controller/index.ts
@@ -1,3 +1,4 @@
 import updateMilestone from './updateMilestone';
+import updateMilestoneStatus from './updateMilestoneStatus';
 
-export default { updateMilestone };
+export default { updateMilestone, updateMilestoneStatus };
