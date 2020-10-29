2458b80a4e88eb28f70c8d725d881488ee675fc4
Baek Jiyoung <bjyoung0@gmail.com>
2020-10-29 17:23:44 +0900

    [feat] 마일스톤 조회 비즈니스 로직 작성 - GET milestones API에 따른 마일스톤들을 조회하는 비즈니스 로직 작성 - milestone에 대한 비즈니스 로직 모아둔 index에 export

--- server/src/service/milestone/index.ts
+++ server/src/service/milestone/index.ts
@@ -1,4 +1,5 @@
 import updateMilestone from './updateMilestone';
 import updateMilestoneStatus from './updateMilestoneStatus';
+import getMilestones from './getMilestones';
 
-export default { updateMilestone, updateMilestoneStatus };
+export default { getMilestones, updateMilestone, updateMilestoneStatus };
