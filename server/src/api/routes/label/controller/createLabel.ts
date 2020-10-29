ea071e8253c0f07b8a68389fa8e79687d74bd23e
Eunbin <qpmz1212@naver.com>
2020-10-29 15:34:26 +0900

    [refactor] request body 객체 destruct - request body 안에 어떤 property 있는지 명시해주기 위해 객체 destruct로 수정

--- server/src/api/routes/label/controller/createLabel.ts
+++ server/src/api/routes/label/controller/createLabel.ts
@@ -2,8 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 import labelService from '@service/label';
 
 const createLabel = async (req: Request, res: Response, next: NextFunction) => {
-  const label = req.body;
-  const labelId = await labelService.createLabel(label);
+  const { name, description, color } = req.body;
+  const labelId = await labelService.createLabel({ name, description, color });
   if (!labelId) {
     return res.status(400).json({ result: false });
   }
