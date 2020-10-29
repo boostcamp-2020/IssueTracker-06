b5e0acba72b6d53254426f3e501f5fe89dcc3010
Eunbin <qpmz1212@naver.com>
2020-10-29 14:53:49 +0900

    [feat] label router 등록 - label router index에 등록

--- server/src/api/routes/index.ts
+++ server/src/api/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import issueRouter from './issue';
+import labelRouter from './label';
 
 const router = Router();
 
 router.use('/issue', issueRouter);
+
+router.use('/label', labelRouter);
 
 export default router;
