0b6efb018648109789aca26578365a9fa5d75b06
Baek Jiyoung <bjyoung0@gmail.com>
2020-10-29 15:32:16 +0900

    [feat] comment router 등록 - comment router index에 등록

--- server/src/api/routes/index.ts
+++ server/src/api/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import issueRouter from './issue';
 import labelsRouter from './labels';
+import commentRouter from './comment';
 
 const router = Router();
 
 router.use('/issue', issueRouter);
 router.use('/labels', labelsRouter);
+router.use('/comment', commentRouter);
 
 export default router;
