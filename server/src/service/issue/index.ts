6d0e93b4b5f357a318fe483d513b6e7478e12c21
Eunbin <qpmz1212@naver.com>
2020-10-29 18:05:55 +0900

    [feat] 이슈 assignee, label, milestone 수정 비즈니스 로직 구현 - PATCH issue assignee, label, milestone API 관련 비즈니스 로직 구현 - controller 에서 사용할 수 있도록 issue index 에 export

--- server/src/service/issue/index.ts
+++ server/src/service/issue/index.ts
@@ -1,4 +1,15 @@
 import createIssue from './createIssue';
 import updateIssueTitle from './updateIssueTitle';
+import updateIssueContent from './updateIssueContent';
+import updateIssueAssignee from './updateIssueAssignee';
+import updateIssueLabel from './updateIssueLabel';
+import updateIssueMilestone from './updateIssueMilestone';
 
-export default { createIssue, updateIssueTitle };
+export default {
+  createIssue,
+  updateIssueTitle,
+  updateIssueContent,
+  updateIssueAssignee,
+  updateIssueLabel,
+  updateIssueMilestone,
+};
