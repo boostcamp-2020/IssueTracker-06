8b147a31ab272940a458cf9950c7fe2dc30e4848
eunjeongS2 <dmswjd2340@gmail.com>
2020-10-29 19:01:54 +0900

    [refactor] 각 셀에서 뷰를 업데이트하도록 수정

---
+++ iOS/IssueTracker/IssueTracker/View/IssueDetailCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  IssueDetailCollectionViewCell.swift
+//  IssueTracker
+//
+//  Created by eunjeong lee on 2020/10/28.
+//
+
+import UIKit
+
+class IssueDetailCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var id: UILabel!
+    @IBOutlet weak var writingTime: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var emojiButton: UIButton!
+    @IBOutlet weak var optionButton: UIButton!
+    
+    func configureCell(with comment: Comment) {
+        id.text = comment.user.name
+        writingTime.text = comment.timestamp
+        commentLabel.text = comment.content
+    }
+}
