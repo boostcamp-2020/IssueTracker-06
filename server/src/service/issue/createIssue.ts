2b16b688a9ef7c81c2736ee7678b7bd37646680e
chanyeong <cyjo9603@naver.com>
2020-10-30 11:16:36 +0900

    [fix] create issue service 에 필수 값 조정
    - 이슈 생성 시 마일스톤, 레이블, assignee는 필수 항목이 아니므로 없을 경우를 고려 필수 값 조정

--- server/src/service/issue/createIssue.ts
+++ server/src/service/issue/createIssue.ts
@@ -3,9 +3,9 @@ import Issue from '@models/issue';
 interface IssueProps {
   title: string;
   content: string;
-  milestoneId: number;
-  labelIds: number[];
-  assigneeIds: number[];
+  milestoneId?: number;
+  labelIds?: number[];
+  assigneeIds?: number[];
   userId: number;
 }
 
@@ -25,8 +25,13 @@ const createIssue = async (issue: IssueProps) => {
       user_id: userId,
       is_open: true,
     })) as IssueInstance;
-    await createdIssue.addLabels(labelIds);
-    await createdIssue.addAssignee(assigneeIds);
+
+    if (labelIds) {
+      await createdIssue.addLabels(labelIds);
+    }
+    if (assigneeIds) {
+      await createdIssue.addAssignee(assigneeIds);
+    }
 
     return createdIssue.id;
   } catch (error) {
