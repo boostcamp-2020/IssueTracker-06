a99233a7b507727b1d262e26c459e1fa3183c564
taehee <yskpth91@gmail.com>
2020-11-02 16:38:26 +0900

    필터 선택 화면
    이슈 필터 화면 정적 TableView 구현
    
    #57

---
+++ iOS/IssueTracker/IssueTracker/Controller/FilterSelectViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FilterSelectViewController.swift
+//  IssueTracker
+//
+//  Created by 박태희 on 2020/11/02.
+//
+
+import UIKit
+
+class FilterSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
