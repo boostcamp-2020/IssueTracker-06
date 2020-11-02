413a0742d3a90ddc0281430a5d26d94272f64d7c
eunjeongS2 <dmswjd2340@gmail.com>
2020-11-02 15:02:02 +0900

    [refactor] IssueListCollectionViewCell xib로 변경

--- iOS/IssueTracker/IssueTracker/View/IssueListCollectionViewCell.swift
+++ iOS/IssueTracker/IssueTracker/View/IssueListCollectionViewCell.swift
@@ -2,7 +2,7 @@
 //  IssueListCollectionViewCell.swift
 //  IssueTracker
 //
-//  Created by 박태희 on 2020/10/27.
+//  Created by eunjeong lee on 2020/11/02.
 //
 
 import UIKit
@@ -10,22 +10,26 @@ import UIKit
 class IssueListCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var widthConstraint: NSLayoutConstraint!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var milestone: LabelView!
-    @IBOutlet weak var labelsStackView: LabelsStackView!
-    
+        @IBOutlet weak var titleLabel: UILabel!
+        @IBOutlet weak var descriptionLabel: UILabel!
+        @IBOutlet weak var milestone: LabelView!
+        @IBOutlet weak var labelsStackView: LabelsStackView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
     func setIssue(_ issue: Issue) {
         titleLabel.text = issue.title
         descriptionLabel.text = issue.description
         milestone.text = issue.milestone
         setLabelsStackView(labels: issue.labels)
     }
-    
+
     func setWidth(_ width: CGFloat) {
         widthConstraint.constant = width
     }
-    
+
     private func setLabelsStackView(labels: [Label]) {
         labelsStackView.clear()
         labels.forEach {
