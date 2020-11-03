35480e8ab7bc8a01ed1d235d5d3267f17bc69943
chanyeong <cyjo9603@naver.com>
2020-11-03 17:43:50 +0900

    [chore] emotion 전역 스타일 적용
    - emotion 전역 스타일 및 폰트 적용
    - storybook 전역 스타일 및 폰트 적용

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,3 +1,36 @@
+import React from 'react';
+import { ThemeProvider } from 'emotion-theming';
+import { Global, css } from '@emotion/core';
+import reset from 'emotion-reset';
+
+import theme from '../src/themes';
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      ${reset}
+      body.sb-show-main {
+        margin: 0 !important;
+        padding: 0 !important;
+      }
+      @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css);
+
+      body.sb-show-main * {
+        font-family: 'Spoqa Han Sans', 'Spoqa Han Sans JP', 'Sans-serif' !important;
+      }
+    `}
+  />
+);
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+      <GlobalStyle />
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
   backgrounds: {
     default: 'white',
--- client/src/themes/global.tsx
+++ client/src/themes/global.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import reset from 'emotion-reset';
 
-const GlobalStyle = () => <Global styles={css``} />;
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      ${reset}
+
+      @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css);
+
+      body {
+        font-family: 'Spoqa Han Sans', 'Spoqa Han Sans JP', 'Sans-serif';
+      }
+    `}
+  />
+);
 
 export default GlobalStyle;
