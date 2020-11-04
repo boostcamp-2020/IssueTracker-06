08f6374dc01b55636972030c18c895ff81a85407
Baek Jiyoung <bjyoung0@gmail.com>
2020-11-04 20:01:22 +0900

    [feat] Input 컴포넌트 구현 - Input View 구현 - storybook test 구현
    #69

---
+++ client/src/components/atoms/Input/Input.tsx
@@ -0,0 +1,45 @@
+import React, { FunctionComponent } from 'react';
+import styled from '@themes/styled';
+
+interface Props {
+  placeholder?: string;
+  value: string;
+  type: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const StyledInput = styled.input`
+  width: 100%;
+  font-size: 16px;
+  background-color: ${({ theme }) => theme.palette.BG_COLOR01};
+  padding: 5px 12px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.palette.PRIMARY};
+  border: 1px solid ${({ theme }) => theme.palette.BORDER_COLOR};
+  border-radius: 6px;
+  outline: none;
+
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.palette.LINK_BLUE};
+    outline: none;
+    box-shadow: 0 0 0 3px ${({ theme }) => theme.palette.BLUE_SHADOW};
+  }
+`;
+
+const Input: FunctionComponent<Props> = ({
+  placeholder,
+  value,
+  type,
+  onChange,
+}) => {
+  return (
+    <StyledInput
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export default Input;
