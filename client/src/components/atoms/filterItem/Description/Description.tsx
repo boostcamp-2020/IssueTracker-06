b227d1d80011b5029c56968a48546bc4bf41cec0
Eunbin <qpmz1212@naver.com>
2020-11-05 16:07:47 +0900

    [refactor] filter item label 부분 css 변경 - filter item label margin, display 등 css 변경

--- client/src/components/atoms/filterItem/Description/Description.tsx
+++ client/src/components/atoms/filterItem/Description/Description.tsx
@@ -2,14 +2,13 @@ import React, { FunctionComponent } from 'react';
 import styled from '@themes/styled';
 
 const StyledDescription = styled.div`
-  font-size: 0.4rem;
-  overflow: hidden;
+  font-size: 0.7rem;
   text-overflow: ellipsis;
   color: ${({ theme }) => theme.palette.SECONDARY};
 `;
 
 interface Props {
-  text: string;
+  text?: string;
 }
 
 const Description: FunctionComponent<Props> = ({ text }) => (
