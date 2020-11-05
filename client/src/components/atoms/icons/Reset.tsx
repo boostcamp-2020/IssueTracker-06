3f6f31b6ed839583b09874edfd9bcc33b9bc7379
Baek Jiyoung <bjyoung0@gmail.com>
2020-11-05 10:31:57 +0900

    [refactor] svg 필요 없는 태그 삭제 - svg 파일에서 필요 없는 태그 삭제

--- client/src/components/atoms/icons/Reset.tsx
+++ client/src/components/atoms/icons/Reset.tsx
@@ -2,8 +2,7 @@ import React, { FunctionComponent } from 'react';
 
 const Reset: FunctionComponent = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 30" x="0px" y="0px">
-      <title>x, decline, reject, cancel, delete</title>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 30">
       <path d="M19.707,4.293a1,1,0,0,0-1.414,0L12,10.586,5.707,4.293A1,1,0,0,0,4.293,5.707L10.586,12,4.293,18.293a1,1,0,1,0,1.414,1.414L12,13.414l6.293,6.293a1,1,0,0,0,1.414-1.414L13.414,12l6.293-6.293A1,1,0,0,0,19.707,4.293Z" />
     </svg>
   );
