9bd0a9c05e46d2250420a6eb844432dfba09392a
Eunbin <qpmz1212@naver.com>
2020-11-05 18:49:36 +0900

    [feat] search filter story에 user일 경우 구현 - search filter story에 item user일 경우 molecule 조합하여 organism 구현

--- client/src/components/organisms/SearchFilter/SearchFilter.stories.tsx
+++ client/src/components/organisms/SearchFilter/SearchFilter.stories.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { text } from '@storybook/addon-knobs';
 import { Dropdown } from '@components/atoms/icons';
-import FilterItemLabel from '@/components/molecules/FilterModal/FilterModalButton/filterItem/FilterItemLabel';
+import SelectMenuItemLabel from '@components/molecules/SelectMenuModal/SelectMenuItem/SelectMenuItemLabel';
+import SelectMenuItemUser from '@components/molecules/SelectMenuModal/SelectMenuItem/SelectMenuItemUser';
 import SearchFilter from './SearchFilter';
 
 export default {
@@ -43,21 +44,21 @@ export const Label = () => {
   const label = text('label', 'Label');
   const optionHeader = text('optionHeader', 'Filter by label');
   const options = [
-    <FilterItemLabel
+    <SelectMenuItemLabel
       key="label1"
       swatchColor="#eee"
       title="1번"
       description="1번1번"
     />,
-    <FilterItemLabel key="label2" swatchColor="#ddd" title="2번" />,
-    <FilterItemLabel
+    <SelectMenuItemLabel key="label2" swatchColor="#ddd" title="2번" />,
+    <SelectMenuItemLabel
       key="label3"
       swatchColor="#ccc"
       title="3번"
       description="3번3번"
     />,
-    <FilterItemLabel key="label4" swatchColor="#bbb" title="4번" />,
-    <FilterItemLabel
+    <SelectMenuItemLabel key="label4" swatchColor="#bbb" title="4번" />,
+    <SelectMenuItemLabel
       key="label5"
       swatchColor="#aaa"
       title="5번"
@@ -71,6 +72,63 @@ export const Label = () => {
   };
   const keys = ['label1', 'label2', 'label3', 'label4', 'label5'];
   const input = 'Filter labels';
+  return (
+    <SearchFilter
+      label={label}
+      optionHeader={optionHeader}
+      options={options}
+      Icon={Icon}
+      onClick={onClick}
+      display={display}
+      input={input}
+      keys={keys}
+    />
+  );
+};
+
+export const User = () => {
+  const [display, setDisplay] = useState('none');
+  const label = text('label', 'Author');
+  const optionHeader = text('optionHeader', 'Filter by Author');
+  const options = [
+    <SelectMenuItemUser
+      key="user1"
+      userImgSrc="https://avatars0.githubusercontent.com/u/37282087?s=40&amp;v=4"
+      title="user1"
+      alt="user1"
+    />,
+    <SelectMenuItemUser
+      key="user2"
+      userImgSrc="https://avatars0.githubusercontent.com/u/37282087?s=40&amp;v=4"
+      title="user2"
+      alt="user2"
+    />,
+    <SelectMenuItemUser
+      key="user3"
+      userImgSrc="https://avatars0.githubusercontent.com/u/37282087?s=40&amp;v=4"
+      title="user3"
+      alt="user3"
+    />,
+    <SelectMenuItemUser
+      key="user4"
+      userImgSrc="https://avatars0.githubusercontent.com/u/37282087?s=40&amp;v=4"
+      title="user4"
+      alt="user4"
+    />,
+    <SelectMenuItemUser
+      key="user5"
+      userImgSrc="https://avatars0.githubusercontent.com/u/37282087?s=40&amp;v=4"
+      title="user5"
+      alt="user5"
+    />,
+  ];
+  const Icon = Dropdown;
+  const onClick = () => {
+    if (display === 'none') setDisplay('block');
+    else setDisplay('none');
+  };
+  const keys = ['user1', 'user2', 'user3', 'user4', 'user5'];
+  const input = 'Filter users';
   return (
     <SearchFilter
       label={label}
