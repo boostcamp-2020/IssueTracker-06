2f59e21d7b52659ccff906a7a8d7b200ebfff7ea
eunjeongS2 <dmswjd2340@gmail.com>
2020-11-09 23:28:23 +0900

    [feat] LabelAddView 구현
    제목: TextFieldInputView, 설명: TextViewInputView, 색상: ColorInputView

---
+++ iOS/IssueTracker/IssueTracker/View/LabelAddView.swift
@@ -0,0 +1,40 @@
+//
+//  LabelAddView.swift
+//  IssueTracker
+//
+//  Created by eunjeong lee on 2020/11/09.
+//
+
+import UIKit
+
+class LabelAddView: UIView {
+
+    private let addView = AddView()
+    private let colorInputView = ColorInputView(propertyName: "색상")
+    private let titleInputView = TextFieldInputView(propertyName: "제목")
+    private let descriptionInputView = TextViewInputView(propertyName: "설명")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureView()
+    }
+    
+    private func configureView() {
+        addView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(addView)
+        addView.setConstraintToFit(at: self)
+        addView.dataSource = self
+    }
+}
+
+extension LabelAddView: AddViewDataSource {
+    
+    func contentViews(_ addView: AddView) -> [ContentInputView] {
+        return [titleInputView, descriptionInputView, colorInputView]
+    }
+}
