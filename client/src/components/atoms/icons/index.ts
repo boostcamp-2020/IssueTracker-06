9ddddb92e01972ef8c66cfc5aa9797e216db24c5
Baek Jiyoung <bjyoung0@gmail.com>
2020-11-09 18:01:25 +0900

    [refactor] filter 수정 - search filter -> filter 로 변경 - filter list component로 구현

--- client/src/components/atoms/icons/index.ts
+++ client/src/components/atoms/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as Reset } from './Reset';
 export { default as Dropdown } from './Dropdown';
 export { default as Warning } from './Warning';
+export { default as Change } from './Change';
