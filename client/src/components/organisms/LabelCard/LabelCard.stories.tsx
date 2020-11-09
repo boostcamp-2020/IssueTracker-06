b25e5289094e400c0906e653265e7d9a4657a638
Baek Jiyoung <bjyoung0@gmail.com>
2020-11-09 18:03:05 +0900

    [feat] label card component 구현 - label card component 구현

---
+++ client/src/components/organisms/LabelCard/LabelCard.stories.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { text, color } from '@storybook/addon-knobs';
+import PlainText from '@components/atoms/PlainText';
+import LabelCard from './LabelCard';
+
+export default {
+  component: LabelCard,
+  title: 'Organisms/LabelCard',
+};
+export const Default = () => {
+  const title = text('title', 'Bug');
+  const bgColor = color('background color', 'rgba(163,10,10,100)');
+  const hexColor = bgColor
+    .slice(5, -1)
+    .split(',')
+    .map((v) => Number(v).toString(16).padStart(2, '0'))
+    .join('')
+    .slice(0, 6);
+  const description = text('description', 'api develop');
+  return (
+    <LabelCard
+      title={title}
+      bgColor={`#${hexColor}`}
+      description={description}
+    />
+  );
+};
