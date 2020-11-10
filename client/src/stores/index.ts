b38e064e55cfd7d9332ffe504d7f014ce0971791
Eunbin <qpmz1212@naver.com>
2020-11-10 20:57:31 +0900

    [feat] issue 목록 담을 store 구현 - issue 목록의 상태를 가지고 있는 store 구현

---
+++ client/src/stores/index.ts
@@ -0,0 +1,43 @@
+import { createContext, Dispatch } from 'react';
+import { Issue } from './type';
+import { AddIssueActions, AddIssueAction, ADD_ISSUE_SUCCESS } from './addIssue';
+
+export type Actions = AddIssueActions;
+export type ActionCreator = AddIssueAction;
+
+interface IssuesState {
+  issues: Issue[];
+}
+
+interface IssuesContext extends IssuesState {
+  dispatch: Dispatch<Actions>;
+  asyncDispatch?: (
+    action: ActionCreator,
+    apiCallback: (requestActions: Actions) => Promise<any>,
+  ) => Promise<void>;
+}
+
+export const initialIssues: IssuesState = {
+  issues: [],
+};
+
+export const reducer = (
+  state: IssuesState = initialIssues,
+  action: Actions,
+): IssuesState => {
+  switch (action.type) {
+    case ADD_ISSUE_SUCCESS: {
+      const { createdIssue } = action;
+      return {
+        issues: [...state.issues, createdIssue],
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default createContext<IssuesContext>({
+  ...initialIssues,
+  dispatch: () => {},
+});
