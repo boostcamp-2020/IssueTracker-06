d8349f45f62272e2aacc119b71e2dac9d74f75d2
Eunbin <qpmz1212@naver.com>
2020-11-10 20:58:59 +0900

    [feat] issue 생성 api 및 action 구현 - POST issue API 기능 구현 - issue 생성할 수 있는 action 구현

---
+++ client/src/apis/createIssue.ts
@@ -0,0 +1,31 @@
+import axios from '@lib/axios';
+import convertIssue from '@lib/convertType';
+import { Issue, User, Comment, Milestone, Label } from '@stores/type';
+import { AddIssueRequest } from '@stores/addIssue';
+
+export interface CreateIssue {
+  title: string;
+  content: string;
+  isOpen: boolean;
+  user: User;
+  assignees: User[];
+  comments: Comment[];
+  milestone: Milestone | null;
+  labels: Label[];
+}
+
+const createIssue = async ({ issue }: AddIssueRequest): Promise<any> => {
+  try {
+    const newIssue = convertIssue(issue);
+    const response = await axios.post('/issue', newIssue);
+    const createdIssue: Issue = {
+      id: response.data,
+      ...issue,
+    };
+    return createdIssue;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default createIssue;
