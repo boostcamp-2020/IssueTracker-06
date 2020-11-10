e839f82a43b7461ed82b8c4f1261a2286361c582
Eunbin <qpmz1212@naver.com>
2020-11-10 21:02:53 +0900

    [feat] issue store app에 전역상태 등록 - issue store app에 전역 상태로 등록

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,21 +1,29 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useReducer, useMemo } from 'react';
 import { ThemeProvider } from 'emotion-theming';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import IssueContext, { reducer, initialIssues } from '@stores/.';
 
 import theme from '@themes/index';
 import GlobalStyle from '@themes/global';
 
 import Main from '@components/pages/Main';
+import useAsync from './hooks/useAsync';
 
-const App: FunctionComponent = () => (
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Main} />
-      </Switch>
-      <GlobalStyle />
-    </BrowserRouter>
-  </ThemeProvider>
-);
+const App: FunctionComponent = () => {
+  const [issues, dispatch] = useReducer(reducer, initialIssues);
+  const asyncDispatch = useMemo(() => useAsync(dispatch), [dispatch]);
+  return (
+    <ThemeProvider theme={theme}>
+      <IssueContext.Provider value={{ ...issues, dispatch, asyncDispatch }}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Main} />
+          </Switch>
+          <GlobalStyle />
+        </BrowserRouter>
+      </IssueContext.Provider>
+    </ThemeProvider>
+  );
+};
 
 export default App;
