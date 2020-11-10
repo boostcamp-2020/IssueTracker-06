3982942150ef4d1e8ef347d0a4ca48d6e017f97d
Eunbin <qpmz1212@naver.com>
2020-11-10 20:56:02 +0900

    [feat] useAsync 커스텀 hook 구현 - API 요청할 async dispatch 만들어주는 custom hook 구현

---
+++ client/src/hooks/useAsync.ts
@@ -0,0 +1,24 @@
+import { Dispatch } from 'react';
+import { ActionCreator, Actions } from '@stores/.';
+
+const useAsync = (dispatch: Dispatch<Actions>) => async (
+  action: ActionCreator,
+  apiCallback: (requestActions: Actions) => Promise<any>,
+): Promise<void> => {
+  try {
+    const requestAction = action.request();
+    dispatch(requestAction);
+    const data = await apiCallback(requestAction);
+
+    if (data) {
+      dispatch(action.success(data));
+      return;
+    }
+    dispatch(action.failure('error'));
+  } catch (error) {
+    console.error(error);
+    dispatch(action.failure(error.message));
+  }
+};
+
+export default useAsync;
