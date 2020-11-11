a1c65f45b988c0f643b663be20256a4e1c254a77
taehee <yskpth91@gmail.com>
2020-11-12 01:25:56 +0900

    [feat] 이슈 상태 변경 구현
    이슈 디테일 뷰에서 바텀뷰의 이슈 상태 버튼 클릭 시 이슈 상태 변경

--- iOS/IssueTracker/IssueTracker/Model/DetailIssueDataManager.swift
+++ iOS/IssueTracker/IssueTracker/Model/DetailIssueDataManager.swift
@@ -51,11 +51,25 @@ struct DetailIssueDataManager {
         })
     }
     
+    func patchIssueStatus(
+        id: Int,
+        body: DetailIssue.IssueStatus,
+        successHandler: ((Bool) -> Void)? = nil,
+        errorHandler: ((Error) -> Void)? = nil) {
+        guard let url = URL(string: "\(IssueTrackerURL.issueState)/\(id)/status") else { return }
+        HTTPServiceHelper.shared.patch(url: url, body: body, successHandler: {
+            successHandler?($0)
+        }, errorHandler: { error in
+            errorHandler?(error)
+        })
+    }
+    
 }
 
 private extension DetailIssueDataManager {
     enum IssueTrackerURL {
         static let patchIssue: String = "http://issue-tracker.cf/api/issue"
         static let issue: String = "http://issue-tracker.cf/api/issue"
+        static let issueState: String = "http://issue-tracker.cf/api/issue"
     }
 }
