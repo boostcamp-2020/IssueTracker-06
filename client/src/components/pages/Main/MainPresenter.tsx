b6a84454e08d02aa85611ea0b52dfc91b85b7dcf
chanyeong <cyjo9603@naver.com>
2020-11-11 10:55:14 +0900

    [feat] AppLayout 메인 페이지에 적용
    - 기본 레이아웃 정의

--- client/src/components/pages/Main/MainPresenter.tsx
+++ client/src/components/pages/Main/MainPresenter.tsx
@@ -1,5 +1,17 @@
 import React, { FunctionComponent } from 'react';
 
-const MainPresenter: FunctionComponent = () => <>Init Page</>;
+import styled from '@themes/styled';
+import AppLayout from '@components/organisms/AppLayout';
+
+const StyledMainSection = styled.section`
+  width: 1200px;
+  margin: 60px auto;
+`;
+
+const MainPresenter: FunctionComponent = () => (
+  <AppLayout>
+    <StyledMainSection>test</StyledMainSection>
+  </AppLayout>
+);
 
 export default MainPresenter;
