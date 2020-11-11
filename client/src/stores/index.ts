e99992bdd24a3efd32c905c4fbae5fcf5fda706e
Eunbin <qpmz1212@naver.com>
2020-11-12 02:56:18 +0900

    [feat] store 내 issue 디렉토리 추가로 인한 수정 - store 내 issue 디렉토리 추가함에 따라 경로 수정 및 기존 index 파일 수정

--- client/src/stores/index.ts
+++ client/src/stores/index.ts
@@ -1,43 +1,5 @@
-import { createContext, Dispatch } from 'react';
-import { Issue } from './type';
-import { AddIssueActions, AddIssueAction, ADD_ISSUE_SUCCESS } from './addIssue';
+import { AddIssueAction, AddIssueActions } from './issue/addIssue';
+import { ReadLabelsAction, ReadLabelsActions } from './label/readLabels';
 
-export type Actions = AddIssueActions;
-export type ActionCreator = AddIssueAction;
-
-interface IssuesState {
-  issues: Issue[];
-}
-
-interface IssuesContext extends IssuesState {
-  dispatch: Dispatch<Actions>;
-  asyncDispatch?: (
-    action: ActionCreator,
-    apiCallback: (requestActions: Actions) => Promise<any>,
-  ) => Promise<void>;
-}
-
-export const initialIssues: IssuesState = {
-  issues: [],
-};
-
-export const reducer = (
-  state: IssuesState = initialIssues,
-  action: Actions,
-): IssuesState => {
-  switch (action.type) {
-    case ADD_ISSUE_SUCCESS: {
-      const { createdIssue } = action;
-      return {
-        issues: [...state.issues, createdIssue],
-      };
-    }
-    default:
-      return state;
-  }
-};
-
-export default createContext<IssuesContext>({
-  ...initialIssues,
-  dispatch: () => {},
-});
+export type Actions = AddIssueActions | ReadLabelsActions;
+export type ActionCreator = AddIssueAction | ReadLabelsAction;
