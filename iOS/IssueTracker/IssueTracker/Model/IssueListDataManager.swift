70ec74f63ace2769fdd785f5ffd84951b5c23c1a
eunjeongS2 <dmswjd2340@gmail.com>
2020-11-12 01:02:43 +0900

    [feat] 이슈 상태 변경 구현

--- iOS/IssueTracker/IssueTracker/Model/IssueListDataManager.swift
+++ iOS/IssueTracker/IssueTracker/Model/IssueListDataManager.swift
@@ -21,11 +21,34 @@ struct IssueListDataManager {
             errorHandler?($0)
         })
     }
+    
+    func updateStatus(
+        id: Int,
+        status: Bool,
+        successHandler: (() -> Void)? = nil,
+        errorHandler: ((Error) -> Void)? = nil) {
+        guard let url = URL(string: IssueTrackerURL.issueStatusURL(id: id)) else { return }
+        let body = [Constant.isOpen: status]
+        HTTPServiceHelper.shared.patch(url: url, body: body, successHandler: { _ in
+            successHandler?()
+        },
+        errorHandler: {
+            errorHandler?($0)
+        })
+    }
 }
 
 private extension IssueListDataManager {
     
     enum IssueTrackerURL {
         static let issues: URL? = URL(string: "http://issue-tracker.cf/api/issues")
+        
+        static func issueStatusURL(id: Int) -> String {
+            "http://issue-tracker.cf/api/issue/\(id)/status"
+        }
+    }
+    
+    enum Constant {
+        static let isOpen = "isOpen"
     }
 }
