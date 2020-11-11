5bc5e053e014ec854f544cfbae86ce3bca6ee309
Eunbin <qpmz1212@naver.com>
2020-11-12 02:47:27 +0900

    [feat] milestone store 구현 - GET milestone API로 milestone store 구현 - API 요청할 수 있는 action, dispatch 구현

---
+++ client/src/stores/milestone/readMilestones.ts
@@ -0,0 +1,44 @@
+import { Milestone } from '@stores/type';
+
+export const READ_MILESTONES_REQUEST = 'READ_MILESTONES_REQUEST';
+export const READ_MILESTONES_SUCCESS = 'READ_MILESTONES_SUCESS';
+export const READ_MILESTONES_FAILURE = 'READ_MILESTONES_FAILURE';
+
+export interface ReadMilestonesRequest {
+  type: typeof READ_MILESTONES_REQUEST;
+}
+
+export interface ReadMilestonesSuccess {
+  type: typeof READ_MILESTONES_SUCCESS;
+  milestones: Milestone[];
+}
+
+export interface ReadMilestonesFailure {
+  type: typeof READ_MILESTONES_FAILURE;
+  error: string;
+}
+
+export interface ReadMilestonesAction {
+  request: () => ReadMilestonesRequest;
+  success: (milestones: Milestone[]) => ReadMilestonesSuccess;
+  failure: (error: string) => ReadMilestonesFailure;
+}
+
+export type ReadMilestonesActions =
+  | ReadMilestonesRequest
+  | ReadMilestonesSuccess
+  | ReadMilestonesFailure;
+
+const readMilestonesAction = (): ReadMilestonesAction => ({
+  request: (): ReadMilestonesRequest => ({ type: READ_MILESTONES_REQUEST }),
+  success: (milestones: Milestone[]): ReadMilestonesSuccess => ({
+    type: READ_MILESTONES_SUCCESS,
+    milestones,
+  }),
+  failure: (error: string): ReadMilestonesFailure => ({
+    type: READ_MILESTONES_FAILURE,
+    error,
+  }),
+});
+
+export default readMilestonesAction;
