d2916ab0425ce41fa39af894607a363ad535a060
Eunbin <qpmz1212@naver.com>
2020-11-12 16:01:08 +0900

    [feat] 프론트 정적파일 경로 서버에 지정 - 프론트 배포위해 정적파일 경로 서버에 지정함

--- server/src/loader/express.ts
+++ server/src/loader/express.ts
@@ -5,6 +5,8 @@ import hpp from 'hpp';
 import cors from 'cors';
 import compression from 'compression';
 import apiRouter from '@api/routes';
+import path from 'path';
+import ejs from 'ejs';
 
 const prod = process.env.NODE_ENV === 'production';
 
@@ -13,8 +15,12 @@ const expressLoader = (app: Express) => {
     app.use(logger('combined'));
     app.use(helmet());
     app.use(hpp());
+    app.set('views', path.join(__dirname, '..', '..', '..', 'client', 'dist'));
+    app.use(express.static(path.join(__dirname, '..', '..', '..', 'client', 'dist')));
   } else {
     app.use(logger('dev'));
+    app.set('views', path.join(__dirname, '..', '..', '..', 'client', 'public'));
+    app.use(express.static(path.join(__dirname, '..', '..', '..', 'client', 'public')));
   }
   app.use(
     cors({
@@ -22,10 +28,15 @@ const expressLoader = (app: Express) => {
       credentials: true,
     }),
   );
+  app.set('view engine', 'ejs');
+  app.engine('html', ejs.renderFile);
   app.use(compression());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use('/api', apiRouter);
+  app.get('/*', (req, res) => {
+    res.render('index.html');
+  });
 };
 
 export default expressLoader;
